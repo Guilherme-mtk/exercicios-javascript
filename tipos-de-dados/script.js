f3e244f5899168d10b1a7013d33d88a4e82fe6a4
Guilherme Lima <76075799+Guilherme-mtk@users.noreply.github.com>
2021-02-20 08:46:29 -0300

    exercicios de tipos de dados

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,22 @@
+ // Qual o resultado da seguinte expressão 
+ var total = 10 + 5 * 2 / 2 + 20;
+  //resultado: 35
+
+  // Crie duas expressões que retornem NaN
+  var execircio = 'volei' - 2;
+  var execircio1 = 'basquete' / 2;
+
+  // Somar a string '200' com o número 50 e retorna 250
+  var calculo = +'200' + 50;
+ 
+ // incremente o numero 5 e retorne o seu valor incrementado
+  var y = 5;
+  ++y;
+  
+  
+  // como dividir o peso por 2?
+  var numero = +'80' / 2;
+  var unidade = 'kg';
+  var peso = numero + unidade; // '80kg'
+
+  console.log(peso);
---
+++ variaveis/script.js
@@ -0,0 +1,22 @@
+// Declarar uma variável com o nome 
+var nome = 'Guilherme';
+
+
+// Declarar uma variável com a idade 
+var idade = 21;
+
+
+// Declarar uma variável com sua comida 
+// favortia e não atribuir valor 
+var comidaFavorita;
+
+// Atribuir valor a comida favorita
+
+var comidaFavorita = 'hamburguer';
+
+
+// Declarar 5 variaveis diferentes sem valores
+
+var cidade, profissao, estado, idioma, naturalidade;
+
+console.log (nome,idade,comidaFavorita);
