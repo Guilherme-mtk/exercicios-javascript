e2f228e6e0bdfc676e00c6cb746d11cd6386c8a1
Guilherme Lima <76075799+Guilherme-mtk@users.noreply.github.com>
2021-02-22 19:35:42 -0300

    tudo é objeto exercicio

---
+++ tudo é objeto/script.js
@@ -0,0 +1,20 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'guilherme';
+//fixed
+//length
+//anchor
+
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('click');
+
+//hasPointerCapture
+//addEventListener
+//attachShadow
+//appendChild
+//animate
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+//new ClipboardJS('.btn');
